836c0ebbeaf343de6aaecaa53690e2a05d25c153
minsooKim <ms.kim8717@gmail.com>
2021-11-05 17:04:25 +0900

    [django] add_trace 로 그래프 그리기

--- graph/views.py
+++ graph/views.py
@@ -8,44 +8,33 @@ import numpy as np
 
 
 def index(request):
-    # Generating some data for plots.
-    x = [i for i in range(-10, 11)]
-    y1 = [3*i for i in x]
-    y2 = [i**2 for i in x]
-    y3 = [10*abs(i) for i in x]
+    np.random.seed(1)
 
-    # List of graph objects for figure.
-    # Each object will contain on series of data.
-    graphs = []
+    N = 100
+    random_x = np.linspace(0, 1, N)
+    random_y0 = np.random.randn(N) + 5
+    random_y1 = np.random.randn(N)
+    random_y2 = np.random.randn(N) - 5
 
-    # Adding linear plot of y1 vs. x.
-    graphs.append(
-        go.Scatter(x=x, y=y1, mode='lines', name='Line y1')
-    )
+    fig = go.Figure()
 
-    # Adding scatter plot of y2 vs. x.
-    # Size of markers defined by y2 value.
-    graphs.append(
-        go.Scatter(x=x, y=y2, mode='markers', opacity=0.8,
-                   marker_size=y2, name='Scatter y2')
-    )
-
-    # Adding bar plot of y3 vs x.
-    graphs.append(
-        go.Bar(x=x, y=y3, name='Bar y3')
-    )
+    # Add traces
+    fig.add_trace(go.Scatter(x=random_x, y=random_y0,
+                             mode='markers',
+                             name='markers'))
+    fig.add_trace(go.Scatter(x=random_x, y=random_y1,
+                             mode='lines+markers',
+                             name='lines+markers'))
+    fig.add_trace(go.Scatter(x=random_x, y=random_y2,
+                             mode='lines',
+                             name='lines'))
 
     # Setting layout of the figure.
-    layout = {
-        'title': 'Title of the figure',
-        'xaxis_title': 'X',
-        'yaxis_title': 'Y',
-        'height': 420,
-        'width': 560,
-    }
+    fig.update_layout(title='Styled Scatter',
+                      yaxis_zeroline=False, xaxis_zeroline=False)
 
     # Getting HTML needed to render the plot.
-    plot_div = plot({'data': graphs, 'layout': layout},
+    plot_div = plot({'data': fig},
                     output_type='div')
 
     return render(request, 'graph/demo_plot.html',
